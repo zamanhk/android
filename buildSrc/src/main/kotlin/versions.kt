f04f04ffa39958b94679ee223570db6985ecf235
agarcia <agarcia@solidgeargroup.com>
2020-09-17 19:44:44 +0200

    Create versions file to handle KTS

---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,61 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author Abel García de Prada
+ * Copyright (C) 2020 ownCloud GmbH.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+object Versions {
+
+    // SDK
+    const val sdkCompileVersion = 29
+    const val sdkMinVersion = 21
+    const val sdkTargetVersion = 29
+
+    // Android jetpack
+    const val archLifecycleVersion = "2.1.0"
+    const val roomVersion = "2.2.5"
+    const val appCompat = "1.1.0"
+    const val sqliteVersion = "2.1.0"
+    const val lifecycleLiveData = "2.2.0"
+    const val biometricVersion = "1.0.1"
+
+    // Kotlin
+    const val kotlinVersion = "1.3.72"
+    const val coroutinesVersion = "1.3.2"
+
+    // Koin
+    const val koinVersion = "2.0.1"
+    const val mockkVersion = "1.9.3"
+
+    // Testing
+    const val junitVersion = "4.12"
+    const val extJunitVersion = "1.1.1"
+    const val androidTestVersion = "1.2.0"
+    const val mockitoVersion = "2.24.0"
+    const val espressoTestVersion = "3.2.0"
+    const val fragmentTestVersion = "1.1.0"
+    const val uiAutomatorTestVersion = "2.2.0"
+    const val annotationTestVersion = "1.1.0"
+
+    // Extensions
+    const val ktxVersion = "1.1.0"
+
+    // KTX extensions
+    const val ktxCoreVersion = "1.3.0"
+    const val ktxViewModelVersion = "2.2.0"
+    const val ktxFragmentVersion = "1.2.4"
+    const val ktxLiveData = "2.1.0"
+}
